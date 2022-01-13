4c43ec8b6bc97ae49046851aa431e7554f174a99
rio-macy <2717860092@qq.com>
2022-01-14 00:44:57 +0800

    第三次提交

--- gitdemo/src/main/java/com/gti/demo/gitdemo/controller/GitController.java
+++ gitdemo/src/main/java/com/gti/demo/gitdemo/controller/GitController.java
@@ -1,7 +1,17 @@
 package com.gti.demo.gitdemo.controller;
 
+import com.gti.demo.gitdemo.service.GitService;
+import com.gti.demo.gitdemo.service.impl.GitServiceImpl;
+
 /**
  * @author sujuntao
  */
 public class GitController {
+
+    GitService gitService = new GitServiceImpl();
+
+    public String push() {
+
+        return "push success";
+    }
 }
