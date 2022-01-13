085a3e1826a691f87eaca5c4a3630857af9502f4
rio-macy <2717860092@qq.com>
2022-01-14 00:07:04 +0800

    第一次更改

---
+++ gitdemo/src/main/java/com/gti/demo/gitdemo/GitDemoApplication.java
@@ -0,0 +1,16 @@
+package com.gti.demo.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sujuntao
+ */
+@SpringBootApplication
+public class GitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoApplication.class, args);
+    }
+
+}
---
+++ gitdemo/src/test/java/com/gti/demo/gitdemo/GitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gti.demo.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
