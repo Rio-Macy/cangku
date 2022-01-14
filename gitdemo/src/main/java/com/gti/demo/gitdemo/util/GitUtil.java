c79eafe14fed05203462eb6c5624dac71aabd04e
xiaocan100226@hlxd.com <2717860092@qq.com>
2022-01-14 10:21:58 +0800

    第十次提交

---
+++ gitdemo/src/main/java/com/gti/demo/gitdemo/util/GitUtil.java
@@ -0,0 +1,17 @@
+package com.gti.demo.gitdemo.util;
+
+/**
+ * @author sujuntao
+ */
+public class GitUtil {
+
+    public static String push() {
+
+        return "push success";
+    }
+
+    public static String commit() {
+
+        return "push success";
+    }
+}
