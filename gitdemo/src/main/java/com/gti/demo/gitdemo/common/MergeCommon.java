2c0505f9beae9a92c68a26085b73151812b3eb87
sujuntao <2717860092@qq.com>
2022-01-15 14:07:46 +0800

    第26次提交,测试提交

--- gitdemo/src/main/java/com/gti/demo/gitdemo/common/MergeCommon.java
+++ gitdemo/src/main/java/com/gti/demo/gitdemo/common/MergeCommon.java
@@ -8,4 +8,9 @@ public class MergeCommon {
     public static void play() {
         System.out.println();
     }
+
+    public static void back() {
+
+        System.out.println("back...");
+    }
 }
